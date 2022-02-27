7bfb0d42cf666cefc6eed57038fe96d534239abb
Tatiana Belova <belousovatanya1995@yandex.ru>
2022-02-27 13:57:30 +0400

    Do first part of task on java

--- app/src/main/java/course/androidhomework/MainActivity.kt
+++ app/src/main/java/course/androidhomework/MainActivity.kt
@@ -1,13 +1,18 @@
 package course.androidhomework
 
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         workWithBooks()
-        workWithUsers()
+//        workWithUsers()
+        BooksJava().workWithBooksJava()
+//        workWithUsersJava()
     }
 }
