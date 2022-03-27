9e1fa30a1b34e58dea2d11881bad4934a870ee09
Tatiana Belova <belousovatanya1995@yandex.ru>
2022-03-27 18:42:47 +0400

    Done homework 8

---
+++ app/src/main/java/course/androidhomework/MyFirebaseMessagingService.kt
@@ -0,0 +1,12 @@
+package course.androidhomework
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        Log.d("MyTag", message.notification.toString())
+    }
+}
